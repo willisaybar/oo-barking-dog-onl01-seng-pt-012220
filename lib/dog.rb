35689eb3c52e6c0de906981ba2f52fd5eb0e5dbf
Willis Aybar <willisaybar@gmail.com>
2020-02-05 01:17:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 # Your code goes here!
 class Dog 
+
 def  name=(dog_name)
   @this_dogs_name = dog_name
 end
@@ -7,13 +8,11 @@ end
   def name
     @name
   end
-    
-end
-
-class Dog 
+  
   def bark
     puts "woof!"
   end
+    
 end
 
 fido = Dog.new
